4d35f6564689d6bb1643e44092e325148e40d26a
Anup <anupsingh841208@gmail.com>
2023-11-16 12:37:34 +0530

    Simplifying

--- index.js
+++ index.js
@@ -3,40 +3,33 @@ const { GoogleAuth } = require("google-auth-library");
 
 const MODEL_NAME = "models/chat-bison-001";
 
-var API_KEY = null;
-async function main(query) {
-    const client = new DiscussServiceClient({
-        authClient: new GoogleAuth().fromAPIKey(API_KEY),
-    });
-    let first = query;
-    let messages = [{ content: first }];
+class PaLM{
+    constructor(){
+        this.messages = [];
+    }
+    setAPIKey(API_key){
+        this.API_KEY = API_key;
+    }
+    async main(query) {
+        const client = new DiscussServiceClient({
+            authClient: new GoogleAuth().fromAPIKey(this.API_KEY),
+        });
+        let messages = [...this.messages, { content: query }];
 
-    const result = await client.generateMessage({
-        model: MODEL_NAME,
-        prompt: { messages },
-    });
+        const result = await client.generateMessage({
+            model: MODEL_NAME,
+            prompt: { messages },
+        });
+        this.messages.push({ content: result[0].candidates[0].content});
+        return result[0].candidates[0].content;
+    }
 
-    //   console.log("User:\n\n", first, "\n\n")
-    //   console.log("Palm:\n\n", result[0].candidates[0].content, "\n\n");
+    PalmHelper(query) {
+        this.main(query).then((res) => {
+            return res;
+        });
+    }
 
-    //   let second = "Oh, where do those live?"
-
-    messages.push({ content: result[0].candidates[0].content });
-    messages.push({ content: second });
-    return messages[1].content;
-
-    //   const secondResult = await client.generateMessage({
-    //     model: MODEL_NAME,
-    //     prompt: { messages },
-    //   });
-
-    //   console.log("User:\n\n", second, "\n\n")
-    //   console.log("Palm:\n\n", secondResult[0].candidates[0].content, "\n\n");
 }
 
-function PalmHelper(API_key, query) {
-    API_KEY = API_key;
-    return main(query);
-}
-
-module.exports = PalmHelper;
+module.exports = new PaLM;
